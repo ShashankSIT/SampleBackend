b0e330fa9817e641423296e04743c23461ac75b1
ShashankKirad <shashank.k@shaligraminfotech.com>
2024-09-11 11:26:50 +0530

    testing cicd

--- SampleBackend.API/Controllers/LoginApiController.cs
+++ SampleBackend.API/Controllers/LoginApiController.cs
@@ -318,9 +318,9 @@ namespace SampleBackend.API.Controllers
             return response;
         }
 
-        [HttpPost("ValidateTwoFactorCode")]
+        [HttpPost("ValidateTwoFactorCodee")]
         [AllowAnonymous]
-        public async Task<ApiPostResponse<LoginModel>> ValidateTwoFactorCode([FromBody] UserAuthModel model)
+        public async Task<ApiPostResponse<LoginModel>> ValidateTwoFactorCodeeee([FromBody] UserAuthModel model)
         {
             ApiPostResponse<LoginModel> response = new();
             try
